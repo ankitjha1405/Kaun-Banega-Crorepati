57f25c332aab278942e86d216eb3ac0ae5f708b4
Ankit Kumar Jha <76553358+ankitjha1405@users.noreply.github.com>
2021-02-08 00:19:36 +0530

    Create kbc.cpp

---
+++ kbc.cpp
@@ -0,0 +1,544 @@
+#include <iostream>
+using namespace std;
+int question=1;
+char answer;
+int score;
+void question1();
+void question2();
+void question3();
+void question4();
+void question5();
+void question6();
+void question7();
+void question8();
+void question9();
+void question10();
+void question11();
+void question12();
+void question13();
+void question14();
+void question15();
+void question16();
+void question17();
+void question18();
+void question19();
+void question20();
+
+
+int main()
+{
+    cout<<"Welcome to Kaun Banegi Crorepati"<<endl;
+    cout<<"To start a new game ";
+    system("PAUSE");
+    score=0;
+    question1();
+    question2();
+    question3();
+    question4();
+    question5();
+    question6();
+    question7();
+    question8();
+    question9();
+    question10();
+    question11();
+    question12();
+    question13();
+    question14();
+    question15();
+    question16();
+    question17();
+    question18();
+    question19();
+    question20();
+
+
+
+
+
+    
+    cout<<"Congratulations! You had answered all the questions"<<endl;
+    cout<<"You Completed this game"<<endl;
+    cout<<"Your final score is:"<<score<<endl;
+    return 0;
+}
+
+
+void question1()
+{
+    cout << "Q " << question << " The International Literacy Day is observed on" << endl;
+    cout << "           A. Sep 8                B. Nov 28" << endl;
+    cout << "           C. May 2                D. Sep 22" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'a' || answer == 'A')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+
+         
+    }
+
+}
+
+
+void question2()
+{
+    cout << "Q " << question <<" The language of Lakshadweep. a Union Territory of India, is"<<endl;
+    cout << "           A. Tamil                 B. Hindi" << endl;
+    cout << "           C. Malyalam              D. Telugu" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'c' || answer == 'C')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question3()
+{
+    cout << "Q " << question << " In which group of places the Kumbha Mela is held every twelve years?" << endl;
+    cout << "           A. Ujjain. Purl; Prayag. Haridwar                       B. Prayag. Haridwar, Ujjain,. Nasik" << endl;
+    cout << "           C. Rameshwaram. Purl, Badrinath. Dwarika                D. Chittakoot, Ujjain, Prayag,'Haridwar" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'b' || answer == 'B')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+void question4()
+{
+    cout << "Q " << question << " Bahubali festival is related to" << endl;
+    cout << "           A. Islam                  B. Hinduism" << endl;
+    cout << "           C. Buddhism               D. jainism" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'd' || answer == 'D')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question5()
+{
+    cout << "Q " << question << " Which day is observed as the World Standards  Day?" << endl;
+    cout << "           A. June 26                B. Oct 14" << endl;
+    cout << "           C. Nov 15                 D. Dec 2" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'b' || answer == 'B')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question6()
+{
+    cout << "Q " << question << " Which of the following was the theme of the World Red Cross and Red Crescent Day?" << endl;
+    cout << "         A. Dignity for all - focus on women         B. Dignity for all - focus on Children" << endl;
+    cout << "         C. Focus on health for all                  D. Nourishment for all-focus on children" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'b' || answer == 'B')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question7()
+{
+    cout << "Q " << question << " September 27 is celebrated every year as" << endl;
+    cout << "           A. Teacher's day                 B. National Integration Day" << endl;
+    cout << "           C. World Tourism Day             D. International Literacy Day" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'c' || answer == 'C')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question8()
+{
+    cout << "Q " << question << " Who is the author of 'Manas Ka-Hans' ?" << endl;
+    cout << "           A. Khushwant Singh                B. Prem Chand" << endl;
+    cout << "           C. Jayashankar Prasad             D. Amrit Lal Nagar" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'd' || answer == 'D')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+
+void question9()
+{
+    cout << "Q " << question << " The death anniversary of which of the following leaders is observed as Martyrs' Day?" << endl;
+    cout << "           A. Smt. Indira Gandhi              B. PI. Jawaharlal Nehru" << endl;
+    cout << "           C. Mahatma Gandhi                  D. Lal Bahadur Shastri" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'c' || answer == 'C')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question10()
+{
+    cout << "Q " << question << " Who is the author of the epic 'Meghdoot'?" << endl;
+    cout << "           A. Vishakadatta                B. Valmiki" << endl;
+    cout << "           C. Banabhatta                D. Kalidas" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'd' || answer == 'D')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question11()
+{
+    cout << "Q " << question << " 'Good Friday' is observed to commemorate the event of" << endl;
+    cout << "           A. birth of Jesus Christ                   B. birth of' St. Peter" << endl;
+    cout << "           C. crucification 'of Jesus Christ          D. rebirth of Jesus Christ" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'c' || answer == 'C')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question12()
+{
+    cout << "Q " << question << " Who is the author of the book 'Amrit Ki Ore'?" << endl;
+    cout << "           A. Mukesh Kumar                B. Narendra Mohan" << endl;
+    cout << "           C. Upendra Nath                D. Nirad C. Choudhary" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'b' || answer == 'B')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question13()
+{
+    cout << "Q " << question << " Which of the following is observed as Sports Day every year?" << endl;
+    cout << "           A. 22nd April               B. 26th  july" << endl;
+    cout << "           C. 29th August              D. 2nd October" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'c' || answer == 'C')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question14()
+{
+    cout << "Q " << question << " World Health Day is observed on" << endl;
+    cout << "           A. Apr 7                B. Mar 6" << endl;
+    cout << "           C. Mar 15               D. Apr 28" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'a' || answer == 'A')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question15()
+{
+    cout << "Q " << question << " Pongal is a popular festival of which state?" << endl;
+    cout << "           A. Karnataka                B. Kerala" << endl;
+    cout << "           C. Tamil Nadu               D. Andhra Pradesh" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'c' || answer == 'C')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question16()
+{
+    cout << "Q " << question << " Ghototkach in Mahabharat was the son of" << endl;
+    cout << "           A. Duryodhana                B. Arjuna" << endl;
+    cout << "           C. Yudhishthir                D. Bhima" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'd' || answer == 'D')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question17()
+{
+    cout << "Q " << question << " Which of the following Muslim festivals is based on the 'Holy Quran' ?" << endl;
+    cout << "           A. Id -ul-Zuha               B. Id -ul-Fitr" << endl;
+    cout << "           C. Bakri-id                  D. Moharram" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'a' || answer == 'A')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+void question18()
+{
+    cout << "Q " << question << " Van Mahotsav was started by" << endl;
+    cout << "           A. Maharshi Karve              B. Bal Gangadhar Tiiak" << endl;
+    cout << "           C. K.M, Munshi                 D. Sanjay Gandhi" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'c' || answer == 'C')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question19()
+{
+    cout << "Q " << question << " The first month of the Indian national calendar is" << endl;
+    cout << "           A. Magha                 B. Chaitra" << endl;
+    cout << "           C. Ashadha               D. Vaishakha" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'b' || answer == 'B')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
+
+void question20()
+{
+    cout << "Q " << question << " Which of the following is not a dance from Kerala?" << endl;
+    cout << "           A. Kathakali                  B. Mohiniattam" << endl;
+    cout << "           C. Ottan Thullal              D. Yaksha Gana" << endl;
+    cout<<"Enter the option: ";
+    cin >> answer;
+    question++;
+    if (answer == 'd' || answer == 'D')
+    {
+        cout << "YAY! It's a Correct Answer" << endl<<endl;
+        score = score + 100;
+    }
+    else
+    {
+        cout << "Oh Noo! It's a Wrong Answer!" << endl<<endl;
+        score = score - 20;
+          
+         
+    }
+
+}
+
